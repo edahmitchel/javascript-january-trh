b99fa6c55a46c370fa3f2bce9ba3dfe52b4ab3cb
mitchel <edahmitchel@example.com>
2023-01-23 12:31:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+x = "raphael";
+x = "EDIDIONG";
+console.log(x);
+
+// DECLARING VARIABLES
+// OLD METHOD
+var a = "hello";
+console.log(a);
+
+// new methods
+let b;
+b = 3;
+console.log(b);
+const c = 1;
+// c = 5;
+console.log(c);
+// console
+// prompt
+// alert
+
+// datatypes - properties and methods
+// crud
+// create
+// read
+// update
+// delete
+
+// primitives
+// string
+let str = "string 1";
+let str2 = "string 2";
+let stringThree = `3`;
+let len = str.length;
+let addition = len + 2;
+// properties
+// alert(addition);
+// alert(str2);
+// prompt
+// let name = prompt("please input your name");
+// if (name.length < 8) {
+//   alert("please input a longer name next time");
+// } else {
+//   alert("name accepted");
+// }
+// method
+// let upperStr = str.toUpperCase();
+// console.log(upperStr, str);
+
+// number
+// boolean
+// undefined
+// null
+// big int
+// symbol
+
+// object based
+// arrays
+// objects
+// sets
+
+// operators
+// assignment
+// comparison operators
+// logical operators
+// ternary operator
+
+// flow control
+
+// conditional
+// if/else
+// switch case
+
+// loops
+// for
+// while loop
+// do while loop
